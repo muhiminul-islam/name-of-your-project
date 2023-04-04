9d4fb97d2f158672713b3461da06615f433ae44f
muhiminul-islam <islammuhiminul11@gmail.com>
2023-04-04 15:55:13 +0600

    upto managing the history stack

---
+++ src/main.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/Contact";
+// import Contact from "./routes/contact";
+import EditContact, {
+  action as EditAction,
+} from "./routes/Edit";
+import { action as destroyAction } from "./routes/Destroy";
+import Index from "./routes/Index";
+
+
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: EditAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
